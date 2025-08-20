8be6b35485acf458bd67daeb2f19da48948939f2
Vaibhav Kumar <vaibhavkumar5158@gmail.com>
2025-08-20 22:02:22 +0530

    Day 27 added

---
+++ day-26/Day.26.2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    vector<long long> b(n);
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    long long mx = *max_element(b.begin(), b.end());
+    long long mn = *min_element(b.begin(), b.end());
+
+    long long diff = mx - mn;
+
+    long long cntMin = count(b.begin(), b.end(), mn);
+    long long cntMax = count(b.begin(), b.end(), mx);
+
+    long long ways;
+    if (mx == mn) {
+        ways = n * (n - 1) / 2;  // all pairs are valid
+    } else {
+        ways = cntMin * cntMax;
+    }
+
+    cout << diff << " " << ways << endl;
+    return 0;
+}
