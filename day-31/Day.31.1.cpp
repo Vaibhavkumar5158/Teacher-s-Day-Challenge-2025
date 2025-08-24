99271ca71caa4c899335bd98a5e521bb6422d116
Vaibhav Kumar <vaibhavkumar5158@gmail.com>
2025-08-25 00:11:07 +0530

    Day 31 added

---
+++ day-31/Day.31.1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    unordered_map<string, int> database;
+
+    while (n--) {
+        string name;
+        cin >> name;
+
+        if (database.find(name) == database.end()) {
+            cout << "OK" << endl;  // using endl here (flushes each time)
+            database[name] = 0;
+        } else {
+            database[name]++;
+            string newName = name + to_string(database[name]);
+            cout << newName << endl;
+            database[newName] = 0;
+        }
+    }
+    return 0;
+}
